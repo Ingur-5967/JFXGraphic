495b9a75fcb67abe16d057fdc45b09f464676093
slava <slava.minasyan2006@mail.ru>
2024-11-04 12:37:01 +0300

    Rewrite methods calculate positions

--- src/main/java/ru/solomka/graphic/WindowCalcHelper.java
+++ src/main/java/ru/solomka/graphic/WindowCalcHelper.java
@@ -1,65 +1,38 @@
 package ru.solomka.graphic;
 
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import lombok.NonNull;
 
 public class WindowCalcHelper {
 
-    public static double calculateTopPosition(Pane node, Node regular) {
-        return getPositiveCentre(node, regular)[1] - (node.getBoundsInLocal().getHeight() / 6.9 * regular.getBoundsInLocal().getHeight() / 2) / 1.5;
+    /**
+     * @param pane    local parent
+     * @param regular target node
+     * @return Centre X coordinate in local parent
+     */
+
+    public static double getCentreX(@NonNull Pane pane, @NonNull AnchorPane regular) {
+        return pane.getPrefWidth() / 2 - regular.getPrefWidth()/2;
     }
 
-    public static double calculateDownPosition(Pane node, Node regular) {
-        return getPositiveCentre(node, regular)[1] + (node.getBoundsInLocal().getHeight() / 6.9 * regular.getBoundsInLocal().getHeight() / 2) / 4.2;
+    /**
+     * @param pane local parent
+     * @param regular target node
+     * @return Centre Y coordinate in local parent
+     */
+
+    public static double getCentreY(@NonNull Pane pane, @NonNull AnchorPane regular) {
+        return pane.getPrefHeight() / 2 - regular.getPrefHeight()/2;
     }
 
-    public static double calculateRightPosition(@NonNull Node node) {
-        return (node.getLayoutBounds().getCenterX() - node.getLayoutBounds().getCenterX() / 2) + node.getBoundsInLocal().getWidth();
-    }
-
-    public static double[] getNegativeCentre(@NonNull Pane node, @NonNull Node regular) {
-        return new double[]{
-                (node.getPrefWidth() / 2 - regular.getBoundsInLocal().getWidth() / 2),
-                (node.getPrefHeight() / 2 - regular.getBoundsInLocal().getHeight() / 2)
-        };
-    }
-
-    public static double[] getNegativeCentreOfLabel(@NonNull Pane node, @NonNull Label regular) {
-
-        node.getPrefWidth();
-        return new double[]{
-                (node.getPrefWidth() / 2 - (regular.getText().length() * regular.getFont().getSize()) / 4),
-                (node.getPrefHeight() / 2 - regular.getFont().getSize()) - 2,
-        };
-
-    }
-
-    public static double[] getPositiveCentre(@NonNull Pane node, @NonNull Node regular) {
-        return new double[]{
-                (node.getPrefWidth() / 2 + regular.getBoundsInLocal().getWidth() / 2),
-                (node.getPrefHeight() / 2 + regular.getBoundsInLocal().getHeight() / 2)
-        };
-    }
-
-    public static double[] getPositiveCentreOfLabel(@NonNull Pane node, @NonNull Label regular) {
-        return new double[]{
-                (node.getPrefWidth() / 2 - regular.getText().length()),
-                (node.getPrefHeight() / 2 - regular.getFont().getSize()) + 1,
-        };
-    }
+    /**
+     * @param node target node
+     * @return Size node (Width, Height)
+     */
 
     public static double[] getSizeComponent(@NonNull Node node) {
         return new double[]{node.getBoundsInParent().getWidth(), node.getBoundsInParent().getHeight()};
-    }
-
-    public static double[] getSizePane(@NonNull AnchorPane pane) {
-        return new double[]{pane.getMaxWidth(), pane.getMaxHeight()};
-    }
-
-    public static double[] getCornerComponent(@NonNull Node node) {
-        return new double[]{node.getBoundsInParent().getMinX(), node.getBoundsInParent().getMinY()};
     }
 }
