39564860302cfa9dfb15c294ffd7d34baa702db1
slava <slava.minasyan2006@mail.ru>
2024-11-10 01:17:15 +0300

    Added new class

---
+++ src/main/java/ru/solomka/graphic/scene/item/impl/text/ExtensibleLabel.java
@@ -0,0 +1,20 @@
+package ru.solomka.graphic.scene.item.impl.text;
+
+import ru.solomka.graphic.scene.item.impl.base.BaseText;
+import ru.solomka.graphic.style.CssStyle;
+
+public class ExtensibleLabel extends BaseText {
+
+    public ExtensibleLabel(String content, int font, int initialCountLines) {
+        super(content, font, initialCountLines);
+        this.getElement().getChildren().add(this.getLineObject(0));
+    }
+
+    @Override
+    public boolean addLine(String content, int font, int padding, CssStyle... styles) {
+        if (this.getMaxLines() < this.getTextData().size())
+            this.setMaxLines(this.getMaxLines() + 1);
+
+        return super.addLine(content, font, padding, styles);
+    }
+}
