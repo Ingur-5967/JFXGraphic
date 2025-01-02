4efdcd5d256b1677abd71ed6b0f6d9779d67aef8
slava <slava.minasyan2006@mail.ru>
2025-01-02 23:48:55 +0300

    Minor changes

--- src/main/java/ru/solomka/graphic/scene/item/impl/text/line/SingleLabel.java
+++ src/main/java/ru/solomka/graphic/scene/item/impl/text/line/SingleLabel.java
@@ -5,6 +5,5 @@ import ru.solomka.graphic.scene.item.impl.base.BaseText;
 public class SingleLabel extends BaseText {
     public SingleLabel(String content, int font) {
         super(content, font);
-        this.getRoot().getChildren().add(this.getLineObject(0));
     }
 }
